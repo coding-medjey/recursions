45476ec9d654479784d1d73bed1165c66b9b674c
Bragatheesh <spvaran123@gmail.com>
2021-02-28 15:54:00 +0530

    reverse an array using recursion - completed

--- recursion.js
+++ recursion.js
@@ -1,12 +1,11 @@
 function reverse(array) {
-  let length = array.length;
-  if ((length === 0)) {
-    return array ;
+  if (!array.length) {
+    return array;
   }
-  console.log(array[length - 1]);
   let a = array.pop();
   reverse(array);
-  return array
+  array.unshift(a);
+  return array;
 }
 
 console.log(reverse([1, 2, 3, 4, 5]));
